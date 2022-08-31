79602210655b726a93b87fcbaeeeebe19c1bec98
Jerome Dumagsang <netflixkenya13@gmail.com>
2022-08-31 19:31:23 +0800

    update

---
+++ client/src/components/AddTransaction.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import { Form } from "react-bootstrap";
+export const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [date, setDate] = useState();
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount,
+      date,
+    };
+
+    addTransaction(newTransaction);
+  };
+
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-control">
+          <label htmlFor="text">Name</label>
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter name..."
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br />
+          </label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Enter amount..."
+          />
+          <label htmlFor="amount">
+            Date <br />
+          </label>
+          <Form.Group controlId="duedate">
+            <Form.Control
+              type="date"
+              name="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              placeholder="date"
+            />
+          </Form.Group>
+        </div>
+        <button className="btn">Add income</button>
+      </form>
+    </>
+  );
+};
